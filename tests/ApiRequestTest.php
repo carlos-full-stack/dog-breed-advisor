d62260e3386502fcf0bf793b46a88a3ccd27e98d
Newbiecodeweaver <carlosgilino@gmail.com>
2023-01-14 10:04:16 +0100

    define ApiRequestTest

---
+++ tests/ApiRequestTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class ApiRequestTest extends KernelTestCase
+{
+    public function testSomething(): void
+    {
+        $kernel = self::bootKernel();
+
+        $this->assertSame('test', $kernel->getEnvironment());
+        // $routerService = static::getContainer()->get('router');
+        // $myCustomService = static::getContainer()->get(CustomService::class);
+    }
+
+    public function testApiRequestGetsAnswerWhenCallAPI()
+    {
+        $apiRequest = new apiRequest;
+        $apiRequest->callApi();
+
+    }
+}
